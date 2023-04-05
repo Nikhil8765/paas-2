689368184744f46c6a86e2871fe87fc8868e3d0d
Nikhil8765 <nikhilmotwani2699@gmail.com>
2023-04-06 00:05:52 +0530

    Initial commit

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDJElvB4zW8A1vdWOUB7uuGNnmNzFnk5jM",
+  authDomain: "adbs-expt-5.firebaseapp.com",
+  databaseURL: "https://adbs-expt-5-default-rtdb.firebaseio.com",
+  projectId: "adbs-expt-5",
+  storageBucket: "adbs-expt-5.appspot.com",
+  messagingSenderId: "288421570354",
+  appId: "1:288421570354:web:0e8f23027138926296592b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
